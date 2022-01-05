aa55f8bedbaa92814a7a15348ee3a8b40f5f476c
Leonardo <leonardo.fg95@gmail.com>
2022-01-05 18:55:37 -0300

    Trabalhando com composer

---
+++ composer/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '9e55d76102e0d0652a866873f93f34e1d7333419',
+        'name' => 'leo/composer',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'leo/composer' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '9e55d76102e0d0652a866873f93f34e1d7333419',
+            'dev_requirement' => false,
+        ),
+        'monolog/monolog' => array(
+            'pretty_version' => '1.0.2',
+            'version' => '1.0.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../monolog/monolog',
+            'aliases' => array(),
+            'reference' => 'b704c49a3051536f67f2d39f13568f74615b9922',
+            'dev_requirement' => false,
+        ),
+    ),
+);
