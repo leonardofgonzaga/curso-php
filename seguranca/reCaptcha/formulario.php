8990020b821bd94c34d24bf1438a0b37aee3978c
Leonardo <leonardo.fg95@gmail.com>
2022-01-06 22:00:43 -0300

    reCaptcha

---
+++ seguranca/reCaptcha/formulario.php
@@ -0,0 +1,6 @@
+<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+<form action="cadastrar.php" method="post">
+    <input type="email" name="inputEmail">
+    <button type="submit">Enviar</button>
+    <div class="g-recaptcha" data-sitekey="6LcY6vYdAAAAAAtDxD1tIQZCDdgfOEoU_Pq2Wv67"></div>
+</form>
